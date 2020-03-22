6baf8233ed4b41040d3d7d1778f4137b5008bf2a
Niki <nikeinikei98@gmail.com>
2020-03-22 03:43:10 +0100

    add debug modules

--- dbg.lua
+++ dbg.lua
@@ -7,7 +7,21 @@ debug.shallowSequenceToString = function(t)
         table.insert(sb, tostring(t[i]))
     end
 
-    return table.concat(sb, ", ")
+    return "[" .. table.concat(sb, ", ") .. "]"
+end
+
+debug.printShallowSequence = function(t)
+    print(debug.shallowSequenceToString(t))
+end
+
+debug.printShallowTable = function(t)
+    local c = {}
+
+    for k, v in pairs(t) do
+        table.insert(c, tostring(k) .. " = " .. tostring(v))
+    end
+
+    print("{" .. table.concat(c, ", ") .. "}")
 end
 
 return debug
