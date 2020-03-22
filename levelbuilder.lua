e31283d726cf7c51532e083c7df2c2de9c1087c7
Niki <nikeinikei98@gmail.com>
2020-03-22 03:45:47 +0100

    add levelbuilder module

---
+++ levelbuilder.lua
@@ -0,0 +1,112 @@
+local enum = require "enum"
+local physicsConstants = require "physicsconstants"
+local json = require "libs.json"
+local Array = require "array"
+
+local LevelBuilder = {}
+
+local Modes = enum {
+    "TerrainBuilding",
+    "RocketStartingLocation",
+    "RocketLandingLocation",
+    "Inspection"
+}
+
+function LevelBuilder:new(name)
+    self.name = name
+    self.terrainPoints = Array()
+    self.rocketStartingLocation = {
+        x = -1000, 
+        y = -1000,
+        w = 200,
+        h = 20
+    }
+    self.rocketLandingLocation = {
+        x = -1000, 
+        y = -1000,
+        w = 200,
+        h = 20
+    }
+
+    self.mode = Modes.TerrainBuilding
+end
+
+function LevelBuilder:mousepressed(mouseX, mouseY, button, istouch, presses)
+    if self.mode == Modes.TerrainBuilding and button == 1 then
+        self.terrainPoints:push(mouseX, mouseY)
+    end
+    if self.mode == Modes.RocketStartingLocation and button == 1 then
+        self.rocketStartingLocation.x = mouseX
+        self.rocketStartingLocation.y = mouseY
+        self.mode = Modes.Inspection
+    end
+    if self.mode == Modes.RocketLandingLocation and button == 1 then
+        self.rocketLandingLocation.x = mouseX
+        self.rocketLandingLocation.y = mouseY
+        self.mode = Modes.Inspection
+    end
+end
+
+function LevelBuilder:keypressed(key, code, isrepeat)
+    if key == "1" then
+        self.mode = Modes.TerrainBuilding
+    end
+    if key == "2" then
+        self.mode = Modes.RocketStartingLocation
+    end
+    if key == "3" then
+        self.mode = Modes.RocketLandingLocation
+    end
+    if key == "4" then
+        self.mode = Modes.Inspection
+    end
+    if key == "s" then
+        local encoded = json.encode(self.terrainPoints:items())
+        local fileName = self.name + ".json"
+    end
+    if key == "escape" then
+        love.event.quit()
+    end
+end
+
+function LevelBuilder:draw()
+    local mouseX, mouseY = love.mouse.getPosition()
+
+    if self.terrainPoints:size() >= 2 then
+        if self.mode == Modes.TerrainBuilding then
+            self.terrainPoints:push(mouseX, mouseY)
+        end
+
+        love.graphics.line(self.terrainPoints:items())
+
+        if self.mode == Modes.TerrainBuilding then
+            self.terrainPoints:pop(2)
+        end
+    end
+
+    local rect = self.rocketStartingLocation
+    if self.mode == Modes.RocketStartingLocation then
+        love.graphics.rectangle("fill", mouseX, mouseY, rect.w, rect.h)
+    else
+        love.graphics.rectangle("fill", rect.x, rect.y, rect.w, rect.h)
+    end
+
+    rect = self.rocketLandingLocation
+    if self.mode == Modes.RocketLandingLocation then
+        love.graphics.rectangle("fill", mouseX, mouseY, rect.w, rect.h)
+    else
+        love.graphics.rectangle("fill", rect.x, rect.y, rect.w, rect.h)
+    end
+
+    love.graphics.print("mode = " .. self.mode)
+end
+
+return setmetatable(LevelBuilder, {
+    __call = function(_, ...)
+        local o = setmetatable({}, {
+            __index = LevelBuilder
+        })
+        o:new(...)
+        return o
+    end
+})
--- main.lua
+++ main.lua
@@ -1,7 +1,11 @@
-local Playing = require "playing"
 require "asserts"
 require "application"
+require "globals"
+
+local Playing = require "playing"
+local LevelBuilder = require "levelbuilder"
 
 function love.load()
     Application.pushState(Playing())
+    Application.pushState(LevelBuilder())
 end
