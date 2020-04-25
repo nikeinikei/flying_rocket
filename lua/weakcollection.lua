af6f1cf9507259112e2d2bc1ca27627a23dc98c0
Niki <nikeinikei98@gmail.com>
2020-04-25 05:08:17 +0200

    add weak collection

---
+++ lua/weakcollection.lua
@@ -0,0 +1,46 @@
+local WeakCollection = {}
+
+function WeakCollection:new()
+    print("WeackCollection:new")
+    self.items = setmetatable({}, {
+        __mode = "v"
+    })
+end
+
+function WeakCollection:add(e)
+    local i = 1
+    while true do
+        if self.items[i] == nil then
+            self.items[i] = e
+            break
+        end
+        i = i + 1
+    end
+end
+
+function WeakCollection:forEach(c)
+    for k, v in pairs(self.items) do
+        c(v)
+    end
+end
+
+local WeakCollection__mt = {
+    __index = WeakCollection,
+    __tostring = function(self)
+        local flattened = {}
+        local i = 1
+        for _, v in pairs(self.items) do
+            flattened[i] = tostring(v)
+            i = i + 1
+        end
+        return table.concat(flattened, ", ")
+    end
+}
+
+return setmetatable(WeakCollection, {
+    __call = function(_, ...)
+        local o = setmetatable({}, WeakCollection__mt)
+        o:new(...)
+        return o
+    end
+})
---
+++ src/weakcollection.d.ts
@@ -0,0 +1,4 @@
+interface WeakCollection {
+    add(e: any): void;
+    forEach(consumer: (this:void, e: any) => void): void;
+}
