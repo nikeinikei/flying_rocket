db4949056696c2aa47a237869d9a819e4822b735
Niki <nikeinikei98@gmail.com>
2020-06-12 03:55:48 +0200

    engine flame particles implement #15

--- game/Color4.lua
+++ game/Color4.lua
@@ -1,6 +1,6 @@
 --[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
 require("lualib_bundle");
-__TS__SourceMapTraceBack(debug.getinfo(1).short_src, {["5"] = 1,["6"] = 1,["7"] = 1,["8"] = 7,["9"] = 8,["10"] = 9,["11"] = 10,["12"] = 11,["13"] = 7,["14"] = 17,["15"] = 18,["16"] = 17});
+__TS__SourceMapTraceBack(debug.getinfo(1).short_src, {["5"] = 1,["6"] = 1,["7"] = 1,["8"] = 7,["9"] = 8,["10"] = 9,["11"] = 10,["12"] = 11,["13"] = 7,["14"] = 14,["15"] = 15,["16"] = 16,["17"] = 14,["18"] = 22,["19"] = 23,["20"] = 22});
 local ____exports = {}
 ____exports.Color = __TS__Class()
 local Color = ____exports.Color
@@ -9,7 +9,11 @@ function Color.prototype.____constructor(self, r, g, b, a)
     self.r = r
     self.g = g
     self.b = b
-    self.a = a
+    self.a = a or 1
+end
+function Color.fromBytes(self, r, g, b)
+    local r1, g1, b1 = love.math.colorFromBytes(r, g, b)
+    return __TS__New(____exports.Color, r1, g1, b1)
 end
 function Color.prototype.unpacked(self)
     return self.r, self.g, self.b, self.a
--- src/rocket.ts
+++ src/rocket.ts
@@ -1,3 +1,4 @@
+import { Color } from "./Color4";
 import { ImageUtils } from "./graphics/ImageUtils";
 import { WrappedDrawable } from "./wrappeddrawable";
 
@@ -10,6 +11,7 @@ export class Rocket {
     public static width: number = 50;
     public static height: number = 80;
     public static userData = "Rocket";
+    public static maxEmissionRate = 200;
 
     private body: Body;
     private shape: PolygonShape;
@@ -17,6 +19,8 @@ export class Rocket {
     private rotation: number;
     private thrust: number;
     private rocketDrawable: WrappedDrawable;
+
+    private particleSystems: [ParticleSystem, WrappedDrawable][];
 
     constructor(world: World, x: number, y: number) {
         this.body = love.physics.newBody(world, x, y, "dynamic");
@@ -31,6 +35,48 @@ export class Rocket {
             ImageUtils.scaleImageToDimensions(rocketImage, Rocket.width, Rocket.height)
         );
         [this.rocketDrawable.ox, this.rocketDrawable.oy] = [Rocket.width / 2, Rocket.height / 2];
+
+        const shadesOfRed = [
+            Color.fromBytes(255, 160, 122),
+            Color.fromBytes(250, 128, 114),
+            Color.fromBytes(233, 150, 122),
+            Color.fromBytes(255, 160, 122),
+            Color.fromBytes(240, 128, 128),
+            Color.fromBytes(205, 92, 92),
+            Color.fromBytes(220, 20, 60),
+            Color.fromBytes(178, 34, 34),
+            Color.fromBytes(255, 0, 0),
+            Color.fromBytes(139, 0, 0),
+            Color.fromBytes(128, 0, 0),
+            Color.fromBytes(255, 99, 71),
+            Color.fromBytes(255, 69, 0),
+        ];
+
+        const width = 4;
+        const height = 4;
+
+        let imageData = love.image.newImageData(4, 4);
+        for (let i = 0; i < width; i++) {
+            for (let j = 0; j < height; j++) {
+                const randomColor = shadesOfRed[love.math.random(shadesOfRed.length) - 1];
+                imageData.setPixel(i, j, ...randomColor.unpacked());
+            }
+        }
+
+        this.particleSystems = [];
+        for (let i = 0; i < 3; i++) {
+            let particleSystem = love.graphics.newParticleSystem(love.graphics.newImage(imageData));
+            particleSystem.setParticleLifetime(0.4, 0.8);
+            particleSystem.setEmissionRate(0);
+            particleSystem.setLinearAcceleration(-10, 500, 10, 700);
+            particleSystem.setColors(1, 1, 1, 1, 1, 1, 1, 0);
+
+            let drawable = new WrappedDrawable(particleSystem);
+            drawable.oy = -Rocket.height / 2;
+            drawable.ox = -Rocket.width / 2 + (i * Rocket.width) / 2;
+
+            this.particleSystems.push([particleSystem, drawable]);
+        }
     }
 
     getBody() {
@@ -63,15 +109,27 @@ export class Rocket {
         let effectiveThrustX = math.sin(angle) * effectiveThrust;
         let effectiveThrustY = -math.cos(angle) * effectiveThrust;
         this.body.applyForce(effectiveThrustX, effectiveThrustY);
+
+        const targetEmissionRate = this.thrust * Rocket.maxEmissionRate;
+        for (const [particleSystem, _] of this.particleSystems) {
+            const currentEmissionRate = particleSystem.getEmissionRate();
+            const diff = targetEmissionRate - currentEmissionRate;
+            const rate = 7;
+
+            particleSystem.setEmissionRate(currentEmissionRate + diff * rate * dt);
+            particleSystem.update(dt);
+        }
     }
 
     draw() {
-        love.graphics.setColor(0.7, 0.7, 0.7, 1);
-        love.graphics.polygon("fill", this.body.getWorldPoints(...this.shape.getPoints()));
-        love.graphics.setColor(1, 1, 1, 1);
-
         [this.rocketDrawable.x, this.rocketDrawable.y] = this.body.getPosition();
         this.rocketDrawable.r = this.body.getAngle();
         this.rocketDrawable.draw();
+
+        for (const [_, particleSystemDrawable] of this.particleSystems) {
+            [particleSystemDrawable.x, particleSystemDrawable.y] = this.body.getPosition();
+            particleSystemDrawable.r = this.body.getAngle();
+            particleSystemDrawable.draw();
+        }
     }
 }
