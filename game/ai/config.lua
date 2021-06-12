5114b7faeb85acdba199e5e8327df7967d80c69c
niki <nikeinikei98@gmail.com>
2021-06-12 19:55:43 +0200

    remove compiled files from git tracking

--- game/ai/config.lua
+++
@@ -1,6 +0,0 @@
---[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
-require("lualib_bundle");
-__TS__SourceMapTraceBack(debug.getinfo(1).short_src, {["5"] = 1});
-local ____exports = {}
-____exports.config = {frameQuotient = 3}
-return ____exports
--- game/controls.lua
+++
@@ -1,6 +0,0 @@
---[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
-require("lualib_bundle");
-__TS__SourceMapTraceBack(debug.getinfo(1).short_src, {["5"] = 11});
-local ____exports = {}
-____exports.Controls = {game = {applyThrust = "space", rotateRight = "right", rotateLeft = "left"}}
-return ____exports
